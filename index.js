c81bbae68cb787bddbb887fb382ae79e6928f1ac
overpowered07 <125555936+overpowered07@users.noreply.github.com>
2023-03-12 13:10:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+// Acknowledgement
+var name = "Acknowledgement-  @Flaticon, @Pixabay, @Youtube, @W3Schools, @Gdevelop"; //string
+console.log(name); //output
+
+// Coordinates
+var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      x.innerHTML = "User denied the request for Geolocation."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML = "Location information is unavailable."
+      break;
+    case error.TIMEOUT:
+      x.innerHTML = "The request to get user location timed out."
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML = "An unknown error occurred."
+      break;
+  }
+}
